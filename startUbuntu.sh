7c025f323b6877f3a5500f00ffdc3a91cf013e74
Frédéric Boulanger <frederic.boulanger@universite-paris-saclay.fr>
2021-04-08 18:24:22 +0200

    Better support for Linux where docker must be run with sudo

--- startUbuntu.sh
+++ startUbuntu.sh
@@ -1,25 +1,50 @@
 #!/bin/sh
+# Ubuntu 20.04LTS headless noVNC
+# Connect to http://localhost:6080/
 REPO=fredblgr/
 IMAGE=ubuntu-novnc
 TAG=20.04
 URL=http://localhost:6080
 
-docker run --rm -d \
-		  -p 6080:80 \
-		  -v ${PWD}:/workspace:rw \
-		  -e USERNAME=`id -n -u` -e USERID=`id -u` -e PASSWORD=ubuntu \
-		  -e RESOLUTION=1400x900 \
-		  --name ${IMAGE}-run \
-		  ${REPO}${IMAGE}:${TAG}
+if [[ -z $SUDO_UID ]]
+then
+  # not in sudo
+  USER_ID=`id -u`
+  USER_NAME=`id -n -u`
+else
+  # in a sudo script
+  USER_ID=${SUDO_UID}
+  USER_NAME=${SUDO_USER}
+fi
+
+docker run --rm --detach \
+  --publish 6080:80 \
+  --volume ${PWD}:/workspace:rw \
+  --env USERNAME=${USER_NAME} --env USERID=${USER_ID} \
+  --env RESOLUTION=1400x900 \
+  --name ${IMAGE} \
+  ${REPO}${IMAGE}:${TAG}
+
 sleep 5
 
-if command -v open 2>&1 /dev/null
+if [[ -z $SUDO_UID ]]
 then
-  open ${URL}
-elif command -v xdg-open 2>&1 /dev/null
-then
-  xdg-open "${URL}"
+     open -a firefox http://localhost:6080 \
+  || xdg-open http://localhost:6080 \
+  || echo "Point your web browser at http://localhost:6080"
 else
-  echo "# Point your browser at ${URL}"
-  echo "You may install xdg-utils so that I can do that for you."
+     su ${USER_NAME} -c 'open -a firefox http://localhost:6080' \
+  || su ${USER_NAME} -c 'xdg-open http://localhost:6080' \
+  || echo "Point your web browser at http://localhost:6080"
 fi
+
+# if command -v open 2>&1 /dev/null
+# then
+#   open ${URL}
+# elif command -v xdg-open 2>&1 /dev/null
+# then
+#   xdg-open "${URL}"
+# else
+#   echo "# Point your browser at ${URL}"
+#   echo "You may install xdg-utils so that I can do that for you."
+# fi
